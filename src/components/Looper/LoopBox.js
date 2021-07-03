111f53ec229ea4c4da0736be388fb40840810e24
John <john.n.pham2000@gmail.com>
2021-07-03 15:50:56 -0400

    Clicking on New Loop will spawn a new progression, no form to creat one just yet

--- src/components/Looper/LoopBox.js
+++ src/components/Looper/LoopBox.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
 import {
 	Avatar,
 	Button,
@@ -16,22 +16,53 @@ import AddIcon from '@material-ui/icons/Add';
 
 import ProgLoop from './ProgLoop'
 
+
 const loopTemp = [
 	{
+		placement: 0,
 		name: "Verse",
-		chords: ["Am, C, B, F"]
+		chords: ["Am", "C", "B", "F"]
 	},
 	{
+		placement: 1,
 		name: "Chorus",
-		chords: ["Gm, C7, Bm, F#m"]
+		chords: ["Gm", "C7", "Bm", "F#m"]
 	},
 	{
+		placement: 2,
 		name: "Bridge",
-		chords: ["Am, C, Bb, Fm"]
+		chords: ["Am", "C", "Bb", "Fm"]
 	}
 ]
 
+
 const LoopBox = () => {
+	const [currProj, setProj] = useState([])
+	
+	// At the moment, this useEffect will undisirably reset changes to the website,
+	// In the future, we'll need this to set the stage for changes
+	// useEffect(() => {
+	// 	// In the final result, this data will come from an API call
+	// 	setProj(loopTemp)
+	// })
+
+	const addNewLoop = () => {
+		console.log("in here")
+		let temp = [...currProj]
+		let len = temp.length
+		temp.push(
+			{
+				placement: len,
+				name: "Another Loop",
+				chords: ["Am", "C", "Bb", "Fm"]
+			}
+		)
+
+		console.log(temp)
+		setProj(temp)
+	}
+
+	
 	return (
 		<div>
 			<Card >
@@ -50,12 +81,13 @@ const LoopBox = () => {
 						title="Project Name"
 						subheader="Created on March 5, 2000"
 					/>
-						<Button variant="contained" color="secondary"><MusicNoteIcon /> New Loop</Button>
+						<Button variant="contained" color="secondary" onClick={addNewLoop}><MusicNoteIcon /> New Loop</Button>
+						<Button variant="contained" color="secondary" onClick={() => console.log(currProj)}><MusicNoteIcon /> Test</Button>
 						
 					
 						<Grid container>
 							{
-								loopTemp.map((loop) => {
+								currProj.map((loop) => {
 									return (
 										<Grid item>
 											<ProgLoop loopData={loop}/>
