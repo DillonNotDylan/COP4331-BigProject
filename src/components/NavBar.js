6b6ca3a05029a821c5c1575cba69f649d4ebbec3
Angel <angel_emmanuel_v@hotmail.com>
2021-07-21 11:01:30 -0400

    adding some verification stuff

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -6,11 +6,13 @@ import {
 	IconButton,
 	Typography,
 	TextField,
+	Link,
+	Dialog,
 } from '@material-ui/core'
 import { makeStyles } from '@material-ui/core/styles'
 import MenuIcon from '@material-ui/icons/Menu';
 import axios from 'axios';
-import { RestoreOutlined } from '@material-ui/icons';
+import { RestoreOutlined, SettingsInputAntenna } from '@material-ui/icons';
 import Login_SignUp from './Login_SignUp';
 import Cookie from "./Cookie"
 
@@ -31,10 +33,11 @@ const useStyles = makeStyles((theme) => ({
 const NavBar = () => {
 	const classes = useStyles();
 	const [lin, setLin] = useState(Cookie.getCookie("userSession") != null);
+	const [passForg, setpassForg] = useState(false);
 	const [user, setUser] = useState("");
 	const [pass, setPass] = useState("");
 	const [errMsg, setErr] = useState("");
-
+	
 	const submitLogin = () => {
 		console.log("User: " + user + "pass:" + pass);
 
@@ -44,7 +47,7 @@ const NavBar = () => {
 			password: pass,
 		};
 
-		var b =axios.post("https://chordeo-grapher.herokuapp.com/user/signin", data)
+		var b = axios.post("https://chordeo-grapher.herokuapp.com/user/signin", data)
         .then(function (response) {
             // if it has response message
 				if (response.data.hasOwnProperty('message'))
@@ -79,6 +82,11 @@ const NavBar = () => {
 		setLin(false);
 	}
 
+	const togglePassReset = () =>
+	{
+		setpassForg(!passForg);
+	}
+
 
 	const formChange = (e) => {
 		// keep track??
@@ -92,20 +100,59 @@ const NavBar = () => {
 
 	}
 
+	const sendPassReset = e =>
+	{
+		e.preventDefault();
+
+		axios.post("https://chordeo-grapher.herokuapp.com/user/signup", {email: user})
+		.then(response =>
+			{
+				// do stuff.
+				// if successful. close out or show that email link sent?
+
+			})
+		.catch(
+			err => console.log("problem sending psw reset = " + err)
+		)
+	}
+
+	const passReset = () =>
+	{
+		return (
+			<div>
+			<Link onClick={()=>togglePassReset()}>Forgot Password?</Link>
+			{
+				passForg?
+				<Dialog open={togglePassReset} onClose={togglePassReset}>
+					<div style={{display:'flex', flexDirection:'column',padding:'10vh', alignItems:'center', justifyContent:'center'}}>
+						<Typography style={{padding:'15px'}}>Forget Password?</Typography>
+						<TextField style={{padding:'30px', width:'20vw'}} placeholder="Enter email to reset password"/>
+						<button style={{padding:'5px'}} onClick={()=>setErr("")}>Reset Password via email</button>
+					</div>
+				</Dialog>
+				:
+				null
+			}
+			</div>
+		)
+	}
+
 
 	const notLoggedIn = () => {
 
 		const clickyStyle = { margin:'5px', textAlign:'center', justifyContent:'center' };
 		return (
 			<div style={{ maxHeight: '5vh', maxWidth: '50vw', display: 'flex', flexDirection: 'row'}}>
-				
-				<Typography style={{marginRight:'20px'}} >{errMsg}</Typography>
+				<Typography style={{marginRight:'20px'}} component={'div'} >
+				{
+					(errMsg.length == 19)? passReset():errMsg
+				}
+				</Typography>
 				<TextField variant="outlined" size="small" placeholder="Username" onChange={formChange} style={clickyStyle} />
 				<TextField variant="outlined" size="small" placeholder="Password" onChange={formChange} style={clickyStyle} />
+				
 				<Login_SignUp buttonText="Sign Up" style={clickyStyle} />
-
 				<Button color="inherit" variant="contained" onClick={submitLogin} style={clickyStyle, {paddingTop:'2%', paddingBottom:'2%'}}>Login</Button>
-
 			</div>
 		);
 	}
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,16 +3,19 @@ import NavBar from '../components/NavBar'
 import PianoPreview from '../components/PianoPreview'
 import ProgLoop from '../components/Looper/ProgLoop'
 import {
+	Dialog,
 	Grid
 } from "@material-ui/core"
 import LoopBox from '../components/Looper/LoopBox'
 import ToolPage from'../components/Tools/ToolPage'
+import AcctVerification from '../components/AcctVerification'
 
 
 const Home = () => {
+
 	return (
 		<div>
-	
+			<AcctVerification/>
 			<NavBar />
 			<PianoPreview />
 			<ToolPage />
